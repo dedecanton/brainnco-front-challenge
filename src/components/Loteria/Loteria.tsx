a7979c4f03000d724fc3e97c2c2999f54f078871
André Canton <andrecanton18@gmail.com>
2022-07-26 15:30:36 -0300

    refactor: code indentation

--- src/components/Loteria/Loteria.tsx
+++ src/components/Loteria/Loteria.tsx
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
+
 import { LoteriaContext } from "../../contexts/Loterias.context";
+
 import { ConcursoType } from "../../types/Loterias.types";
+
 import useFetch from "../../utils/hooks/useFetch";
+
 import LoteriaDetails from "../LoteriaDetails";
 import LoteriaResults from "../LoteriaResults";
 import Spinner from "../Spinner";
+
 const BASE_URL = "https://brainn-api-loterias.herokuapp.com/api/v1";
 
 const Loteria = () => {
@@ -21,15 +27,15 @@ const Loteria = () => {
     BASE_URL + "/concursos/" + concursoId
   ).data;
 
-    useEffect(() => {
-      setIsLoading(true)
-    },[id])
+  useEffect(() => {
+    setIsLoading(true);
+  }, [id]);
 
   useEffect(() => {
     if (concurso) setIsLoading(false);
   }, [concurso]);
 
-  if (isLoading) return <Spinner/>;
+  if (isLoading) return <Spinner />;
   if (!id) return <h1>not id</h1>;
   if (!concurso) return <h1>not concurso</h1>;
   if (!loteria) return <h1>not loteria</h1>;
