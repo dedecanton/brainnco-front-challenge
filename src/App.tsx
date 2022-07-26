c375dec1902fee86c515813800921c0dbd2e0cdc
André Canton <andrecanton18@gmail.com>
2022-07-26 15:27:01 -0300

    feat: loading on loading and loteria component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AppContainer } from "./App.styles";
+import Loteria from "./components/Loteria";
+import Spinner from "./components/Spinner";
+import { LoteriaContext } from "./contexts/Loterias.context";
+
 
 function App() {
-  return <div className="App"></div>;
+  
+  const { loading } = useContext(LoteriaContext)
+  if(loading) return <Spinner/>
+  
+  return (
+    <AppContainer>
+      <Routes>
+        <Route path="/" element={<Navigate to="/0" />} />
+        <Route
+          path=":id"
+          element={<Loteria/>}
+        ></Route>
+      </Routes>
+    </AppContainer>
+  );
 }
 
 export default App;
